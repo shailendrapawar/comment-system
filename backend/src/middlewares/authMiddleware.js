b5bace52e3b3c999f6dab4653bce5875d65984bf
shailendr pawar <shailendrapawar980@gmail.com>
2026-01-21 00:54:19 +0530

    fixes:unauth

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,9 +1,9 @@
 import { verifyToken } from "../helpers/tokenHandler.js";
-
+import { logger } from "../utils.js"
 export const authMiddleware = (req, res, next) => {
     try {
         const token = req.cookies?.token;
-
+        logger.debug(`User came  with token`,req.cookies)
         if (!token) {
             return res.status(401).json({ message: "Unauthorized" });
         }
@@ -13,7 +13,7 @@ export const authMiddleware = (req, res, next) => {
         req.user = {
             _id: payload.sub,
         };
-
+        logger.debug(`User authenticated with token`)
         next();
     } catch (err) {
         return res.status(401).json({ message: "Invalid token" });
