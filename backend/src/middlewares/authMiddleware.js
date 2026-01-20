e16c5f64c1df9004f4597da4a605869868677913
shailendr pawar <shailendrapawar980@gmail.com>
2026-01-21 01:28:52 +0530

    fixes:cookies setting

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -2,6 +2,7 @@ import { verifyToken } from "../helpers/tokenHandler.js";
 import { logger } from "../utils/logger.js"
 export const authMiddleware = (req, res, next) => {
     try {
+        logger.debug(`User requesting through ajuth middlreware`, req.cookies)
         const token = req.cookies?.token;
         logger.debug(`User came  with token`, req.cookies)
         if (!token) {
